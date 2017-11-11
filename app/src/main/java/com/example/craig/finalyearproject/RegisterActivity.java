66f497a981eadfe408e7f9f16ec5c009f31f8b8a
craigc2016 <craigcormack2012@hotmail.com>
2017-11-11 17:37:50 +0000

    Completed full login and register of accounts

--- app/src/main/java/com/example/craig/finalyearproject/RegisterActivity.java
+++ app/src/main/java/com/example/craig/finalyearproject/RegisterActivity.java
@@ -17,7 +17,8 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
     private EditText email;
     private EditText password;
     private Button signIn;
@@ -28,7 +29,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
 
         //Get Reference to Firebase Authentication
         firebaseAuth = FirebaseAuth.getInstance();
@@ -46,6 +47,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         msg = (TextView) findViewById(R.id.txtMsg);
         progressDialog = new ProgressDialog(this);
 
+
         //Add listener to buttons and text view
         signIn.setOnClickListener(this);
         clear.setOnClickListener(this);
@@ -59,25 +61,27 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     public void onClick(View view) {
         if(view == signIn){
-            userLogin();
+            userReg();
+        }
+        else if (view == msg){
+            finish();
+            startActivity(new Intent(this,LoginActivity.class));
         }
         else if(view == clear){
             email.setText("");
             password.setText("");
             email.setFocusable(true);
         }
-        else if(view == msg){
-            RegisterScreen();
-        }
     }
 
-    private void userLogin(){
+    private void userReg(){
         String emailText = email.getText().toString().trim();
         String passwordText = password.getText().toString().trim();
 
+
         if (TextUtils.isEmpty(emailText)){
             //email is empty
-            Toast.makeText(this,"Please enter email", Toast.LENGTH_LONG).show();
+            Toast.makeText(this,"Please enter email",Toast.LENGTH_SHORT).show();
             //stopping the function execution further
             return;
 
@@ -85,30 +89,34 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         if (TextUtils.isEmpty(passwordText)){
             //email is empty
-            Toast.makeText(this,"Please enter password",Toast.LENGTH_LONG).show();
+            Toast.makeText(this,"Please enter password",Toast.LENGTH_SHORT).show();
             //stopping the function execution further
             return;
 
         }
         //If validations are ok
         //show progress bar
-        progressDialog.setMessage("Logging in...");
+        progressDialog.setMessage("Registering User...");
         progressDialog.show();
 
-        firebaseAuth.signInWithEmailAndPassword(emailText,passwordText)
+
+        firebaseAuth.createUserWithEmailAndPassword(emailText,passwordText)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         progressDialog.dismiss();
-                        if(task.isSuccessful()){
-                            HomeScreen();
-                        }else {
-                            Toast.makeText(getApplicationContext(), "ERROR USER NOT FOUND PLEASE TRY AGAIN!",Toast.LENGTH_SHORT).show();
+                        if (task.isSuccessful()){
+                            //User is successfully registered and logged in
+                            LoginScreen();
+
+                        }
+                        else{
+                            Toast.makeText(RegisterActivity.this, "Failed to register" , Toast.LENGTH_SHORT).show();
+
                         }
 
                     }
                 });
-
     }
 
     protected void onPause(){
@@ -116,13 +124,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         progressDialog.dismiss();
     }
 
-    private void RegisterScreen(){
-        startActivity(new Intent(this,RegisterActivity.class));
-        finish();
-    }
 
-    private void HomeScreen(){
-        startActivity(new Intent(this,HomeActivity.class));
+    private void LoginScreen(){
+        startActivity(new Intent(getApplicationContext(),LoginActivity.class));
         finish();
     }
 
